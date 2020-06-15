906b42785d57e48cb19e4b11805631b3d6b6b0cf
Karthik Selvan <kt.green.098@gmail.com>
2020-06-15 14:41:40 +0530

    Update PinBulkUpload.py

--- PinBulkUpload.py
+++ PinBulkUpload.py
@@ -1,4 +1,4 @@
-# Created by Karthik on 10/03/2020
+# Created by karth on 6/14/2020
 import os
 import glob
 import time
@@ -6,132 +6,88 @@ from selenium import webdriver
 from random import randint
 
 # Your Pinterest account details
-user_mail = "e-mail / username"
-user_password = "password"
+user_mail = input("Enter your username/email: ") 
+user_password = input("Enter your password: ")
 
 # Selenium predefined xpaths and href's do not mess with these predefinition update them if they have expired
-pinterest_home = "https://pinterest.com/"
-pin_builder = "https://in.pinterest.com/pin-builder/"
+pinterest_home = "https://pinterest.ca/"
+pin_builder = "https://www.pinterest.ca/pin-builder/"
 flex_pre_login = '//*[@id="__PWS_ROOT__"]/div[1]/div/div/div/div[3]/div[1]/div[1]/div[2]/div[1]/button/div'
 mail_box = '//*[@id="email"]'
 pass_box = '//*[@id="password"]'
-login_button = '//*[@id="__PWS_ROOT__"]/div[1]/div[2]/div/div/div[3]/div[1]/div/div/div[1]/div/div/div/div[3]/form/div[5]/button/div'
+login_button = '//*[@id="__PWS_ROOT__"]/div[1]/div/div/div/div[3]/div[1]/div/div/div[1]/div/div/div/div[3]/form/div[5]/button'
 pin_name = "//*[starts-with(@id, 'pin-draft-title-')]"
 pin_description = "//*[starts-with(@id, 'pin-draft-description-')]"
 image_input = "media-upload-input"
 drop_down_menu = "//button[@data-test-id='board-dropdown-select-button']"
-publish_button = "//button[@data-test-id='board-dropdown-save-button']"
-
+save_button = "//button[@data-test-id='board-dropdown-save-button']"
 
 # Defining your Pinterest boards
 board_names = []
 boards = []
-ind = 0
-
-# Creating a txt file to store the name of the boards
-os.chdir('G:\Python')
-
-# To add your board names to the list by reading them from the txt file
-def addBoardList():
-    file = open('Boards.txt','r')
-    for itr in file:
-        board_names.append(itr.rstrip())
-    file.close()
-
-# Add your board to the txt file if not shown in the list
-def addBoard():
-    file = open('Boards.txt', 'a')
-    file.write('\n' + input("Enter your desired board name **attention this name is case sensitive**: "))
-    file.close()
-    confirmation()
-
-# Show the availabele boards in the list
-def boardChoice():
-    global ind
-    file = open('Boards.txt', 'r')
-    print(file.read())
-    file.close()
-    decision = input("Do you have your board listed Y/N?: ")
-    if decision.casefold() == 'y':
-        addBoardList()
-        bname = input("Enter the board of your choice from the list shown **attention this name case sensitive**: ")
-        for itr in range (len(board_names)):
-            if bname == board_names[itr]:
-                ind = itr
-        boardFormat()
-    else:
-        addBoard()
-
-# A chance to correct the txt file if last entry is wrong
-def correctBoard():
-    file = open("Boards.txt", "r")
-    fromTxt_file = file.read()
-    file.close()
-    To_duplicate_list = fromTxt_file.split("\n")
-    Remove_last_element = "\n".join(To_duplicate_list[:-1])
-    file = open("Boards.txt", "w+")
-    for itr in range(len(Remove_last_element)):
-        file.write(Remove_last_element[itr])
-    file.close()
-    addBoard()
-
-# To confirm if the entry is correct
-def confirmation():
-    global ind
-    print("Make sure you have entered the name correctly")
-    decision = input("Do you want to make any changes Y/N?: ")
-    if decision.casefold() == 'y':
-        correctBoard()
-    else:
-        addBoardList()
-        time.sleep(2)
-        print(board_names)
-        ind = (len(board_names)-1)
-        boardFormat()
-
-# To append the board names to the required format for the selenium
-def boardFormat():
-    for itr in board_names:
-        boards.append("//*[@title='{0}']".format(itr))
-
-# Choosing your board to pin the images
-boardChoice()
+index = 0
 
 # Definitions about pins
 image_file = 'D:\My album\PinBulk\\'
-description = input("Enter a description for your pins **attention this goes for each every pin in the folder**: ")
+description = input("Enter a description for your pins **attention this goes for each and every pin in the folder**: ")
 
 #changing directory to the images folder
 os.chdir('D:\My album\PinBulk')
 
 #Getting images from file
 image_list = []
-for filename in glob.glob('*.jpg'):
-    image_list.append(filename)
+Extension = ['*.png','*.jpg']
+for i in Extension:
+    for filename in glob.glob(i):
+        image_list.append(filename)
 
 # Defining chrome driver
 driver = webdriver.Chrome('C:\WebDrivers\chromedriver')
 
+# Creating a list of available boards
+def boardList():
+    boards_webelement_list = driver.find_elements_by_xpath('//div[@class="tBJ dyH iFc yTZ pBj DrD IZT mWe z-6"]')
+    for board in boards_webelement_list:
+        board_names.append(board.get_attribute("innerHTML"))
+
+# Prompting the selection of board from the user
+def boardChoice():
+    global index
+    boardList()
+    for num in range(len(board_names)):
+        print(str(num)+"-"+board_names[num])
+    choice = int(input("Enter the number associated with your selected board: "))
+    if choice>=0 and choice<=(len(board_names))-1:
+        index = choice
+    else:
+        boardChoice()
+    boardFormat()
+
+# Converting the board_name to a xpath format
+def boardFormat():
+    for itr in board_names:
+        boards.append("//*[@title='{0}']".format(itr))
+
 # Slicing the image name
 def pre_name():
     ind = img.find(".")
-    product = img[:(ind)]
-    pre_name = product.replace("_", " ")
+    find_ind = img[:(ind)]
+    pre_name = find_ind.replace("_", " ")
     pre_name = pre_name.upper()
     return pre_name
 
-# Login to your account
-def login():
-    # Open pinterest on Chrome driver
-    driver.get(pinterest_home)
-    driver.find_element_by_xpath(flex_pre_login).click()
-
-    # Enter the mail and password and login..
-    driver.find_element_by_xpath(mail_box).send_keys(user_mail)
-    driver.find_element_by_xpath(pass_box).send_keys(user_password)
-    time.sleep(2)
-    driver.find_element_by_xpath(login_button).click()
+# To get the boards
+def getBoards():
+    # Go pin builder page
+    driver.get(pin_builder)
     time.sleep(5)
+
+    # Open board drop-down menu
+    driver.find_element_by_xpath(drop_down_menu).click()
+    time.sleep(3)
+
+    # Choosing your board to pin the images
+    boardChoice()
 
 # To pin the image to board
 def pin() :
@@ -158,30 +114,38 @@ def pin() :
     time.sleep(3)
 
     # Select board
-    driver.find_element_by_xpath(board).click()
+    driver.find_element_by_xpath(boards[index]).click()
     time.sleep(3)
 
     # Click publish button
-    driver.find_element_by_xpath(publish_button).click()
+    driver.find_element_by_xpath(save_button).click()
+    time.sleep(5)
+
+# Login to your account
+def login():
+    # Open pinterest on Chrome driver
+    driver.get(pinterest_home)
+    driver.find_element_by_xpath(flex_pre_login).click()
+
+    # Enter the mail and password to login..
+    driver.find_element_by_xpath(mail_box).send_keys(user_mail)
+    driver.find_element_by_xpath(pass_box).send_keys(user_password)
+    time.sleep(2)
+    driver.find_element_by_xpath(login_button).click()
     time.sleep(5)
 
 # Login to your pinterest account
 login()
-i = 0
+getBoards()
+image_count = 0
 
-# when ther are files in the folder the image_list will be > 1
-while i < len(image_list):
+# when there are files in the folder the image_list will be > 1
+while image_count < len(image_list):
     for img in image_list:
         name = pre_name()
-        board = boards[ind]  # choose your board from the list
-        b_ind = -2
+        print("Processing", name.title(), "....")
         pin()
-        i += 1
-        print(name.title(), "pinned on", board[12:(b_ind)])
-        print("{} image(s) are pinned.".format(i))
-        t = randint(0,5)
-        print("Waiting next session...{} seconds".format(t))
-        print("")
-        time.sleep(t)
-print("All done! Finally {} images have been pinned!".format(i))
+        image_count += 1
+        time.sleep(randint(0,5))
+print("Task Completed! Totally {} images have been pinned!".format(image_count))
 driver.quit()
